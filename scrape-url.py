d4a1e0cf93ee9adefa31ad8bfdcada3221264d39
lhes23 <32542078+lhes23@users.noreply.github.com>
2021-10-08 18:41:37 +0800

    added a write csv

--- scrape-url.py
+++ scrape-url.py
@@ -1,7 +1,16 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import csv
 
-html = requests.get('https://onlinetrepreneur.com')
+url = 'https://onlinetrepreneur.com'
+html = requests.get(url)
 soup = bs(html.text,'lxml')
 title = soup.find('title') 
 print(title.text)
+
+data = [[url,title.text]]
+
+with open('scrape-url.csv','w') as writeFile:
+    writer = csv.writer(writeFile)
+    writer.writerows(data)
+writeFile.close()
