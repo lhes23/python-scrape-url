f044f87626fa62c97e9c073866528e0647131e91
lhes23 <32542078+lhes23@users.noreply.github.com>
2021-10-09 21:12:53 +0800

    add 2 sites

--- scrape-url.py
+++ scrape-url.py
@@ -2,23 +2,28 @@ import requests
 from bs4 import BeautifulSoup as bs
 import csv
 
+data = []
+
 def scrapeURL(url):
     html = requests.get(url)
     soup = bs(html.text,'lxml')
     title = soup.find('title')
-    data = []
     data.append([url,title.text])
     return data
 
-def writeCSV(data):
+def writeCSV(records):
     with open('scrape-url.csv','w') as writeFile:
         writer = csv.writer(writeFile)
-        writer.writerows(data)
+        writer.writerows(records)
     writeFile.close()
 
 urls = (
-    'https://onlinetrepreneur.com'
+    'https://onlinetrepreneur.com',
+    'http://agreview.net'
 )
 
-data = scrapeURL(urls)
-writeCSV(data)
+for url in urls:
+    records = scrapeURL(url)
+    writeCSV(records)
+
+print('Successfully Added')
